43b5ced51d8ea7511be8031ba7d37ce0bfc2bfe1
DeepaThangaraju <deepat2405@gmail.com>
2022-03-10 13:46:44 +0530

    login

--- src/screens/LoginPage.js
+++ src/screens/LoginPage.js
@@ -35,6 +35,7 @@ export default function LoginPage({location,history}) {
           <Form.Label>Email</Form.Label>
           <Form.Control
             type="email"
+            id="email"
             placeholder="Enter email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
@@ -42,6 +43,7 @@ export default function LoginPage({location,history}) {
           <Form.Label>Password</Form.Label>
           <Form.Control
             type="password"
+            id="password"
             placeholder="Enter password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
